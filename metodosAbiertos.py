e7badd208b98e71e1c2f66024370f763f09a7c9e
juanda <jdperezs@eafit.edu.co>
2021-10-10 21:26:47 -0500

    solucion de error de llamados falta solucionar raices multiples

--- metodosAbiertos.py
+++ metodosAbiertos.py
@@ -12,12 +12,16 @@ def puntoFijo(g,x0,tol,Nmax):
 def newton(f,x0,tol,Nmax):
     cont=0
     error=tol+1
-    df=f.derivada()
+    df= Funcion(f.derivada())
+    f0= f.evaluar(f.getF(),x0)
+    f1= df.evaluar(df.getF(),x0)
     while(cont<Nmax and error>tol):
-        xn = x0 - (f.evaluar(f.getF(),x0))/(df.evaluar(f.getF(),x0))
+        xn = x0 - (f0)/(f1)
         error = abs(xn-x0)
         cont = cont + 1
         x0 = xn
+        f0= f.evaluar(f.getF(),x0)
+        f1= df.evaluar(df.getF(),x0)
     return [xn,error,cont]
 def secante(f,x0,x1,tol,Nmax):
     cont=0
@@ -32,20 +36,26 @@ def secante(f,x0,x1,tol,Nmax):
 def multiplesRaices(f,x0,tol,Nmax):
     cont=0
     error=tol+1
-    df=f.derivada()
-    df2 = df.derivada()
+    df=Funcion(f.derivada())
+    df2 = Funcion(df.derivada())
+    factual= f.evaluar(f.getF(),x0)
+    fderivada = df.evaluar(df.getF(),x0)
+    fderivada2 =df2.evaluar(df2.getF(),x0)
     while(cont<Nmax and error>tol):
-        xn = x0 - ((f.evaluar(f.getF(),x0))/(df.evaluar(f.getF(),x0))/((df.evaluar(f.getF(),x0))^2)-((f.evaluar(f.getF(),x0))*df2.evaluar(f.getF(),x0)))
+        xn = x0 - ((factual*fderivada)/((fderivada)*fderivada)-((factual)*fderivada2))
         error = abs(xn-x0)
         cont = cont + 1
         x0 = xn
+        factual= f.evaluar(f.getF(),x0)
+        fderivada = df.evaluar(df.getF(),x0)
+        fderivada2 =df2.evaluar(df2.getF(),x0)
     return [xn,error,cont]
 print("introduce la funcion en terminos de x ")
 expr = input()
 x = Funcion(expr)
-a = float(input("introduce el intervalo inicial "))
+a = int(input("introduce el intervalo inicial "))
 b= float(input("introduce el intervalo final "))
 tol= float(input("introduce la tolerancia "))
 Nmax = float(input("introduce las iteraciones maximas "))
-resul= puntoFijo(x,a,tol,Nmax)
+resul= multiplesRaices(x,a,tol,Nmax)
 print(resul)
